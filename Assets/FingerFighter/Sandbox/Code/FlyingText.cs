47ba317d2791b0d2682f5d18a375a97eec6e1058
Kirill Smirnov <thegaldren@gmail.com>
2021-10-13 17:57:52 +0300

    FlyingText.cs alpha changes through time

--- Assets/FingerFighter/Sandbox/Code/FlyingText.cs
+++ Assets/FingerFighter/Sandbox/Code/FlyingText.cs
@@ -9,6 +9,7 @@ namespace FingerFighter.Sandbox
         [SerializeField] private TextMeshPro text;
         [SerializeField] private float duration = 3f;
         [SerializeField] private float speed = 1f;
+        [SerializeField] private AnimationCurve textAlphaCurve;
         
         private Vector2 _direction;
         private float _durationLeft;
@@ -23,12 +24,21 @@ namespace FingerFighter.Sandbox
         private void Update()
         {
             Move();
+            UpdateTextAlpha();
             SubtractTime();
             RepoolIfNeeded();
         }
 
         private void Move() 
             => transform.position = (Vector2) transform.position + _direction * (speed * Time.deltaTime);
+
+        private void UpdateTextAlpha()
+        {
+            var color = text.color;
+            var t = 1 - _durationLeft / duration; 
+            color.a = textAlphaCurve.Evaluate(t);
+            text.color = color;
+        }
 
         private void SubtractTime() 
             => _durationLeft -= Time.deltaTime;
