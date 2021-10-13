28bd4eecdd342208465a634c67c46f469e564234
Kirill Smirnov <thegaldren@gmail.com>
2021-10-13 17:47:38 +0300

    FlyingTextFactory.cs pools entries

--- Assets/FingerFighter/Sandbox/Code/FlyingText.cs
+++ Assets/FingerFighter/Sandbox/Code/FlyingText.cs
@@ -11,18 +11,23 @@ namespace FingerFighter.Sandbox
         [SerializeField] private float speed = 1f;
         
         private Vector2 _direction;
+        private float _durationLeft;
         
         public void Init(string textToShow, Vector2 direction)
         {
             text.text = textToShow;
             _direction = direction;
+            _durationLeft = duration;
         }
 
         private void Update()
         {
             transform.position = (Vector2)transform.position + _direction * (speed * Time.deltaTime);
-            duration -= Time.deltaTime;
-            if(duration <= 0) Destroy(gameObject);
+            _durationLeft -= Time.deltaTime;
+            if (_durationLeft <= 0)
+            {
+                FlyingTextFactory.Instance.ReturnToPool(this);
+            }
         }
     }
 }
--- Assets/FingerFighter/Sandbox/Code/FlyingTextFactory.cs
+++ Assets/FingerFighter/Sandbox/Code/FlyingTextFactory.cs
@@ -1,3 +1,4 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace FingerFighter.Sandbox
@@ -8,6 +9,8 @@ namespace FingerFighter.Sandbox
 
         [SerializeField] private GameObject hitDamageTextPrefab;
         
+        private readonly Queue<FlyingText> _pool = new Queue<FlyingText>();
+        
         private void Awake()
         {
             Instance = this;
@@ -16,15 +19,36 @@ namespace FingerFighter.Sandbox
         public void Instantiate(string text, Vector2 position, Vector2 direction)
         {
             direction = NormalizeDirection(direction);
-            Instantiate(hitDamageTextPrefab, position, Quaternion.identity, transform)
-                .GetComponent<FlyingText>()
-                .Init(text, direction);
+            var newFlyingText = GetNewFlyingText(position);
+            newFlyingText.Init(text, direction);
         }
 
         private static Vector2 NormalizeDirection(Vector2 direction)
         {
             if (direction.sqrMagnitude < 0.001f) direction = new Vector2(Random.Range(0.1f, 1f), Random.Range(0.1f, 1f));
             return direction.normalized;
+        }
+
+        private FlyingText GetNewFlyingText(Vector2 position)
+        {
+            if (_pool.Count > 0)
+            {
+                var newFlyingText = _pool.Dequeue();
+                newFlyingText.transform.position = position;
+                newFlyingText.gameObject.SetActive(true);
+                return newFlyingText;
+            }
+            else
+            {
+                return Instantiate(hitDamageTextPrefab, position, Quaternion.identity, transform)
+                    .GetComponent<FlyingText>();
+            }
+        }
+
+        public void ReturnToPool(FlyingText flyingText)
+        {
+            flyingText.gameObject.SetActive(false);
+            _pool.Enqueue(flyingText);
         }
     }
 }
