e7c5e21011a641d2338605be74e2b6057efd28e5
Kirill Smirnov <thegaldren@gmail.com>
2021-10-15 16:38:39 +0300

    RunnerEnemySpawn.cs

--- Assets/FingerFighter/Code/Control/Camera/JumpObjectOnOutOfCamera.cs
+++ Assets/FingerFighter/Code/Control/Camera/JumpObjectOnOutOfCamera.cs
@@ -4,9 +4,14 @@ namespace FingerFighter.View
 {
     public class JumpObjectOnOutOfCamera : MonoBehaviour
     {
-        [SerializeField] private Vector2 jumpDirection = new Vector2(0, 20);
+        [SerializeField] protected Vector2 jumpDirection = new Vector2(0, 20);
 
-        private void OnTriggerExit2D(Collider2D other)
+        protected virtual void OnTriggerExit2D(Collider2D other)
+        {
+            Jump();
+        }
+
+        protected void Jump()
         {
             transform.position += (Vector3) jumpDirection;
         }
---
+++ Assets/FingerFighter/Code/Control/Factories/RunnerEnemySpawn.cs
@@ -0,0 +1,39 @@
+﻿using FingerFighter.View;
+using UnityEngine;
+
+namespace FingerFighter.Control.Factories
+{
+    // TODO add type to CombatantId 
+    // TODO pool by type
+    public class RunnerEnemySpawn : JumpObjectOnOutOfCamera
+    {
+        [SerializeField] private GameObject enemyPrefab;
+        [SerializeField] private Vector2Int enemyCount = new Vector2Int(3, 10);
+
+        private Transform _anchor;
+        
+        protected override void OnTriggerExit2D(Collider2D other) { }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            SpawnEnemies();
+            Jump();
+        }
+
+        private void Awake()
+        {
+            _anchor = transform.parent;
+        }
+
+        private void SpawnEnemies()
+        {
+            var count = Random.Range(enemyCount.x, enemyCount.y);
+            Vector2 curPos = transform.position;
+            for (int i = 0; i < count; i++)
+            {
+                var pos = curPos + Random.insideUnitCircle * jumpDirection.y / 2;
+                Instantiate(enemyPrefab, pos, Quaternion.identity, _anchor);
+            }
+        }
+    }
+}
