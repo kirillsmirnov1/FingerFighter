fc8166ed5f84ef14ccc8183404265a3fe5a9d3bf
Kirill Smirnov <thegaldren@gmail.com>
2021-10-15 15:50:00 +0300

    PlayerSingleton.cs

---
+++ Assets/FingerFighter/Code/Control/Character/PlayerSingleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.Control.Character
+{
+    public class PlayerSingleton : MonoBehaviour // IMPR 
+    {
+        public static Transform Transform => _instance._transform;
+        
+        private static PlayerSingleton _instance;
+
+        private Transform _transform;
+        
+        private void Awake()
+        {
+            _instance = this;
+            _transform = transform;
+        }
+    }
+}
