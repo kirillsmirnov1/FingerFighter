be8da74b42778fd11e5a021ccf36364ce6c2846d
Kirill Smirnov <thegaldren@gmail.com>
2021-10-15 12:52:00 +0300

    ScenePicker.cs

---
+++ Assets/FingerFighter/Code/Control/Scenes/ButtonLoadsScene.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace FingerFighter.Control.Scenes
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonLoadsScene : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI sceneName;
+
+        private SceneAsset _sceneAsset;
+        
+        public void Init(SceneAsset scene)
+        {
+            _sceneAsset = scene;
+            sceneName.text = scene.name;
+        }
+
+        public void OnClick()
+        {
+            Debug.Log($"Clicked on {_sceneAsset.name} button");
+            SceneManager.LoadScene(_sceneAsset.name);
+        }
+    }
+}
---
+++ Assets/FingerFighter/Code/Control/Scenes/ScenePicker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace FingerFighter.Control.Scenes
+{
+    public class ScenePicker : MonoBehaviour
+    {
+        [SerializeField] private SceneAsset[] scenes;
+        [SerializeField] private GameObject buttonPrefab;
+
+        private void Awake()
+        {
+            SpawnSceneButtons();
+        }
+
+        private void SpawnSceneButtons()
+        {
+            for (int i = 0; i < scenes.Length; i++)
+            {
+                Instantiate(buttonPrefab, transform)
+                    .GetComponent<ButtonLoadsScene>()
+                    .Init(scenes[i]);
+            }
+        }
+    }
+}
