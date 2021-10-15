49847e3b3ea533731c898cd912a79cc0a0fafc47
Kirill Smirnov <thegaldren@gmail.com>
2021-10-15 15:22:25 +0300

    CombatEntityStatus.cs

---
+++ Assets/FingerFighter/Code/Control/Combat/CombatEntityStatus.cs
@@ -0,0 +1,25 @@
+﻿using FingerFighter.Model;
+using FingerFighter.Model.Combat;
+using UnityEngine;
+
+namespace FingerFighter.Control.Combat
+{
+    [RequireComponent(typeof(Health))]
+    [RequireComponent(typeof(CombatEntityId))]
+    public class CombatEntityStatus : MonoBehaviour
+    {
+        [SerializeField] private Health health;
+        [SerializeField] private CombatEntityId id;
+
+        private void OnValidate()
+        {
+            if (health == null) health = GetComponent<Health>();
+            if (id == null) id = GetComponent<CombatEntityId>();
+        }
+
+        private void OnEnable() => health.onNoHealth += OnNoHealth;
+        private void OnDisable() => health.onNoHealth -= OnNoHealth;
+        private void OnNoHealth() => Die();
+        private void Die() => Destroy(gameObject); // IMPR probably should provide it with some referenced behaviour 
+    }
+}
