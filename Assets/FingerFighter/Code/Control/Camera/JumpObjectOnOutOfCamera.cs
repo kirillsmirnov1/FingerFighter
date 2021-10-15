1ca463f7ca4cb40281b066747b1d5aa47a4bf18b
Kirill Smirnov <thegaldren@gmail.com>
2021-10-15 12:14:08 +0300

    Floor moves with camera

---
+++ Assets/FingerFighter/Code/Control/Camera/JumpObjectOnOutOfCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.View
+{
+    public class JumpObjectOnOutOfCamera : MonoBehaviour
+    {
+        [SerializeField] private Vector2 jumpDirection = new Vector2(0, 20);
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            transform.position += (Vector3) jumpDirection;
+        }
+    }
+}
