1a1f787ff6ba0f1bc4bcc5bc3c9ab585004fd976
Kirill Smirnov <thegaldren@gmail.com>
2021-10-15 12:01:34 +0300

    RunnerCameraMovement.cs

---
+++ Assets/FingerFighter/Code/Control/Camera/RunnerCameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.View
+{
+    public class RunnerCameraMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 1f;
+
+        private Transform _transform;
+        private Vector3 _up;
+        
+        private void Awake()
+        {
+            _transform = transform;
+            _up = Vector3.up;
+        }
+
+        private void Update()
+        {
+            _transform.position += _up * (speed * Time.deltaTime);
+        }
+    }
+}
