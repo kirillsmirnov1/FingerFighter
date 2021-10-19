7bc0fdc03a7ab9d5997b9f98958d38e96ef0e402
Kirill Smirnov <thegaldren@gmail.com>
2021-10-19 10:35:54 +0300

    OnPlayerDeathCanvas.prefab

---
+++ Assets/FingerFighter/Code/Control/Combat/OnPlayerDeath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace FingerFighter.Control.Combat
+{
+    public class OnPlayerDeath : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent actions;
+        
+        private void Awake() => PlayerStatus.OnDead += OnDeath;
+        private void OnDestroy() => PlayerStatus.OnDead -= OnDeath;
+        private void OnDeath() => actions.Invoke();
+    }
+}
