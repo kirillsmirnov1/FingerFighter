83703862bfa9187008d3ce9903e1414284d503de
Kirill Smirnov <thegaldren@gmail.com>
2021-10-19 12:37:36 +0300

    Create EnemyFormation.cs

---
+++ Assets/FingerFighter/Code/Model/EnemyFormations/EnemyFormation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.Model.EnemyFormations
+{
+    public class EnemyFormation : MonoBehaviour
+    {
+        private void OnDrawGizmosSelected()
+        {
+            DrawBorderRect();
+        }
+
+        private static void DrawBorderRect()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(Vector3.zero, 10 * Vector3.one);
+        }
+    }
+}
