d993ec8abfb082458c8c1ee2edf36808c8c96663
Kirill Smirnov <thegaldren@gmail.com>
2021-10-19 11:59:08 +0300

    EnemyCollisionHitForce.cs uses stats

---
+++ Assets/FingerFighter/Code/Model/Damage/HitDataProvider/EnemyCollisionHitForce.cs
@@ -0,0 +1,26 @@
+﻿using FingerFighter.Model.Combat;
+using UnityEngine;
+
+namespace FingerFighter.Model.Damage.HitDataProvider
+{
+    public class EnemyCollisionHitForce : AHitDataProvider
+    {
+        [SerializeField] private CombatEntityId id;
+        private HitData _hitData;
+
+        private void OnValidate()
+        {
+            id = GetComponent<CombatEntityId>();
+        }
+
+        private void OnEnable()
+        {
+            _hitData = new HitData
+            {
+                Force = id.EnemyStats.collisionDamage
+            };
+        }
+
+        public override HitData HitData => _hitData;
+    }
+}
