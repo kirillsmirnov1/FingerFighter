83da57d6761cb632dc3a467d606c5ac8fc762e6c
Kirill Smirnov <thegaldren@gmail.com>
2021-10-19 10:48:37 +0300

    OnPlayerDeathCanvas.prefab uses ReloadScene.cs

---
+++ Assets/FingerFighter/Code/Control/Scenes/ReloadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FingerFighter.Control.Scenes
+{
+    public class ReloadScene : MonoBehaviour
+    {
+        public void Reload()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
