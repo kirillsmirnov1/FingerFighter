65145628610f2122247379adba80e51a1b2da74d
Kirill Smirnov <thegaldren@gmail.com>
2021-10-19 16:02:28 +0300

    CombatEntityId.cs uses EnemyStats

--- Assets/FingerFighter/Code/Model/Combat/CombatEntityId.cs
+++ Assets/FingerFighter/Code/Model/Combat/CombatEntityId.cs
@@ -7,43 +7,32 @@ namespace FingerFighter.Model.Combat
     public class CombatEntityId : MonoBehaviour
     {
         [SerializeField] private Affiliation affiliation = Affiliation.Nada;
-        [ConditionalField("affiliation", compareValues:new object[] {Affiliation.Enemy})]
-        [SerializeField] private string enemyType;
-        
-        [ConditionalField("affiliation", compareValues:new object[] {Affiliation.Enemy})] 
-        [SerializeField] private EnemyStatsData stats; // TODO use EnemyStats 
 
-        public string EnemyType => enemyType;
-        public Affiliation Affiliation => affiliation; 
-        public EnemyStats EnemyStats { get; private set; }
+        [ConditionalField("affiliation", compareValues:new object[] {Affiliation.Enemy})] 
+        [SerializeField] private EnemyStats stats;  
+
+        public string EnemyType => stats.tag;
+        public Affiliation Affiliation => affiliation;
+        public EnemyStats EnemyStats => stats;
         
         private void OnValidate()
         {
             Check(AffiliationNotSet && IsNotCombatEntityPrefab, 
                 $"Set affiliation on {gameObject.name}");
             
-            Check(IsEnemy && EnemyTypeNotSet && IsNotBaseEnemyPrefab, 
-                $"Set enemy type on {gameObject.name}");
-            
-            Check(IsEnemy && !EnemyTypeNotSet && NoStatsForThatEnemy, 
+            Check(IsEnemy && IsNotBaseEnemyPrefab && NoStatsForThatEnemy, 
                 $"Provide enemy data for {gameObject.name}");        
         }
 
         private static void Check(bool condition, string warning)
         {
-            if(condition) Debug.LogWarning(warning);
-        }
-
-        private void Awake()
-        {
-            if(IsEnemy) EnemyStats = stats.GetData(enemyType);
+            if (condition) Debug.LogWarning(warning);
         }
 
         private bool AffiliationNotSet => affiliation == Affiliation.Nada;
         private bool IsNotCombatEntityPrefab => gameObject.name != "CombatEntity";
         private bool IsEnemy => affiliation == Affiliation.Enemy;
-        private bool EnemyTypeNotSet => string.IsNullOrEmpty(enemyType);
         private bool IsNotBaseEnemyPrefab => gameObject.name != "_EnemyCombatEntity";
-        private bool NoStatsForThatEnemy => !stats.HasEnemy(enemyType);
+        private bool NoStatsForThatEnemy => stats == null;
     }
 }
