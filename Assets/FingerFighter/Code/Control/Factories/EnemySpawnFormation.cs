ac8a4984ae81078a1c1e77f43820fd1f093fca55
Kirill Smirnov <thegaldren@gmail.com>
2021-10-21 17:09:09 +0300

    EnemySpawnFormation.cs

---
+++ Assets/FingerFighter/Code/Control/Factories/EnemySpawnFormation.cs
@@ -0,0 +1,28 @@
+﻿using FingerFighter.Model.EnemyFormations;
+using UnityEngine;
+
+namespace FingerFighter.Control.Factories
+{
+    public class EnemySpawnFormation : EnemySpawn
+    {
+        [SerializeField] private EnemyFormationPack pack;
+        
+        private int _i;
+        
+        protected override void Spawn()
+        {
+            var formation = NextFormation().entries;
+            for (int i = 0; i < formation.Length; i++)
+            {
+                SpawnEnemy(formation[i].enemy.tag, formation[i].pos);
+            }
+        }
+
+        private EnemyFormation NextFormation()
+        {
+            var formation = pack.Formations[_i];
+            _i = (_i + 1) % pack.Formations.Length;
+            return formation;
+        }
+    }
+}
