a2b4f8d5dd72ad4a6fb7655482fec857c2d3eafa
Kirill Smirnov <thegaldren@gmail.com>
2021-10-21 14:54:40 +0300

    EnemyFormationEntryDrawer.cs

---
+++ Assets/FingerFighter/Code/Editor/Model/EnemyFormations/EnemyFormationEntryDrawer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace FingerFighter.Model.EnemyFormations
+{
+    [CustomPropertyDrawer(typeof(EnemyFormationEntry))]
+    public class EnemyFormationEntryDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, GUIContent.none, property);
+            {
+                var totalWidth = position.width;
+                // Type
+                position.width = 0.25f * totalWidth;
+                EditorGUI.PropertyField(position, property.FindPropertyRelative("enemy"), GUIContent.none);
+                
+                // Gap
+                position.width += 0.01f * totalWidth;
+                
+                // Pos
+                position.x += position.width;
+                position.width = 0.74f * totalWidth;
+                EditorGUI.PropertyField(position, property.FindPropertyRelative("pos"), GUIContent.none);
+            }
+            EditorGUI.EndProperty();
+        }
+    }
+}
