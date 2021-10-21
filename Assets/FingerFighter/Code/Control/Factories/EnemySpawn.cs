8639e90d68a25716dc6175e85dd526f9fbf4f3cf
Kirill Smirnov <thegaldren@gmail.com>
2021-10-21 16:54:34 +0300

    Extract EnemySpawn.cs from EnemySpawnRandomCount.cs

---
+++ Assets/FingerFighter/Code/Control/Factories/EnemySpawn.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using FingerFighter.Model.Combat;
+using FingerFighter.View;
+using UnityEngine;
+
+namespace FingerFighter.Control.Factories
+{
+    public abstract class EnemySpawn : JumpObjectOnOutOfCamera
+    {
+        [SerializeField] protected EnemyPrefabs enemies;
+        
+        private readonly Dictionary<string, GameObject> prefabs = new Dictionary<string, GameObject>();
+        private readonly Dictionary<string, Queue<GameObject>> pool = new Dictionary<string, Queue<GameObject>>();
+        
+        private Transform anchor;
+        private static EnemySpawn _instance;
+        private Vector2 _currentPos;
+
+        private void Awake()
+        {
+            _instance = this;
+            anchor = transform.parent;
+            InitDictionaries();
+        }
+
+        private void InitDictionaries()
+        {
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                pool.Add(enemies[i].tag, new Queue<GameObject>());
+                prefabs.Add(enemies[i].tag, enemies[i].prefab);
+            }
+        }
+
+        protected override void OnTriggerExit2D(Collider2D other) { }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            _currentPos = transform.position;
+            Spawn();
+            Jump();
+        }
+
+        protected abstract void Spawn();
+
+        protected void SpawnEnemy(string enemyTag, Vector2 relativePos)
+        {
+            var pos = _currentPos + relativePos;
+            var instance = GetFromPool(enemyTag);
+            instance.SetActive(true);
+            instance.transform.position = pos;
+        }
+
+        protected GameObject GetFromPool(string enemyTag)
+        {
+            return pool[enemyTag].Count > 0 
+                ? pool[enemyTag].Dequeue() 
+                : Instantiate(prefabs[enemyTag], anchor);
+        }
+
+        public static void ReturnToPool(GameObject obj, string enemyType)
+        {
+            _instance?.pool[enemyType].Enqueue(obj);
+        }
+    }
+}
--- Assets/FingerFighter/Code/Control/Factories/EnemySpawnRandomCount.cs
+++ Assets/FingerFighter/Code/Control/Factories/EnemySpawnRandomCount.cs
@@ -1,72 +1,27 @@
-﻿using System.Collections.Generic;
-using FingerFighter.Model.Combat;
-using FingerFighter.View;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace FingerFighter.Control.Factories
 {
-    public class EnemySpawnRandomCount : JumpObjectOnOutOfCamera
+    public class EnemySpawnRandomCount : EnemySpawn
     {
         [SerializeField] private Vector2Int enemyCount = new Vector2Int(3, 10);
-        [SerializeField] private EnemyPrefabs enemies;
-        
-        private readonly Dictionary<string, GameObject> _prefabs = new Dictionary<string, GameObject>();
-        private readonly Dictionary<string, Queue<GameObject>> _pool = new Dictionary<string, Queue<GameObject>>();
-        
-        private Transform _anchor;
 
-        private static EnemySpawnRandomCount _instance;
-
-        private void Awake()
-        {
-            _instance = this;
-            _anchor = transform.parent;
-            InitDictionaries();
-        }
-
-        private void InitDictionaries()
-        {
-            for (int i = 0; i < enemies.Length; i++)
-            {
-                _pool.Add(enemies[i].tag, new Queue<GameObject>());
-                _prefabs.Add(enemies[i].tag, enemies[i].prefab);
-            }
-        }
-
-        protected override void OnTriggerExit2D(Collider2D other) { }
-
-        private void OnTriggerEnter2D(Collider2D other)
+        protected override void Spawn()
         {
             for (int i = 0; i < enemies.Length; i++)
             {
                 SpawnEnemies(enemies[i].tag);
             }
-            Jump();
         }
 
         private void SpawnEnemies(string enemyTag)
         {
             var count = Random.Range(enemyCount.x, enemyCount.y);
-            Vector2 curPos = transform.position;
             for (int i = 0; i < count; i++)
             {
-                var pos = curPos + Random.insideUnitCircle * jumpDirection.y / 2;
-                var instance = GetFromPool(enemyTag);
-                instance.SetActive(true);
-                instance.transform.position = pos;
+                var relativePos = Random.insideUnitCircle * jumpDirection.y / 2;
+                SpawnEnemy(enemyTag, relativePos);
             }
-        }
-
-        private GameObject GetFromPool(string enemyTag)
-        {
-            return _pool[enemyTag].Count > 0 
-                ? _pool[enemyTag].Dequeue() 
-                : Instantiate(_prefabs[enemyTag], _anchor);
-        }
-
-        public static void ReturnToPool(GameObject obj, string enemyType)
-        {
-            _instance?._pool[enemyType].Enqueue(obj);
         }
     }
 }
