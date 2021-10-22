74316224912d80086c85e2123c4a65dd44e4d29d
Kirill Smirnov <thegaldren@gmail.com>
2021-10-22 13:44:28 +0300

    EnemyStatsList.cs have dictionary tag-stat; initiated

--- Assets/FingerFighter/Code/Model/Combat/EnemyStatsList.cs
+++ Assets/FingerFighter/Code/Model/Combat/EnemyStatsList.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityUtils.Saves; // FIXME shouldn't be in Saves
 
 namespace FingerFighter.Model.Combat
 {
     [CreateAssetMenu(menuName = "Data/EnemyStats", fileName = "EnemyStats", order = 0)]
-    public class EnemyStatsList : ScriptableObject
+    public class EnemyStatsList : InitiatedScriptableObject
     {
         [SerializeField] private List<EnemyStats> stats;
+        
+        private readonly Dictionary<string, EnemyStats> _statsDictionary = new Dictionary<string, EnemyStats>();
 
         public int Count => stats.Count;
-        public EnemyStats this[int i] => stats[i]; 
-        
+        public EnemyStats this[int i] => stats[i];
+        public EnemyStats this[string tag] => _statsDictionary[tag];
+
+        public override void Init() => FillStatsDictionary();
+
+        private void FillStatsDictionary()
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                _statsDictionary.Add(stats[i].tag, stats[i]);
+            }
+        }
+
 #if UNITY_EDITOR
         public void Add()
         {
