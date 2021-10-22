9a292e23593e46e978801d648b56817b647b3530
Kirill Smirnov <thegaldren@gmail.com>
2021-10-22 11:00:11 +0300

    HandlesInputManager.cs picks nearest handle for finger

--- Assets/FingerFighter/Code/Control/Character/Handles/HandlesInputManager.cs
+++ Assets/FingerFighter/Code/Control/Character/Handles/HandlesInputManager.cs
@@ -13,9 +13,10 @@ namespace FingerFighter.Control.Character.Handles
         [SerializeField] private Handle[] handles;
 
         private readonly Dictionary<Finger, Handle> _pairings = new Dictionary<Finger, Handle>();
-        private Queue<Handle> _freeHandles;
+        private List<Handle> _freeHandles;
 
-        private static readonly object Lock = new object(); 
+        private static readonly object Lock = new object();
+        private Camera _camera;
     
         private void OnValidate()
         {
@@ -24,12 +25,13 @@ namespace FingerFighter.Control.Character.Handles
 
         private void Awake()
         {
-            _freeHandles = new Queue<Handle>(handles);
+            _freeHandles = new List<Handle>(handles);
             EnhancedTouchSupport.Enable();
         }
 
         private void OnEnable()
         {
+            _camera = Camera.main;
             Touch.onFingerDown += OnFingerDown;
             Touch.onFingerUp += OnFingerUp;
             PlayerStatus.OnDead += OnPlayerDead;
@@ -47,10 +49,32 @@ namespace FingerFighter.Control.Character.Handles
             lock (Lock)
             {
                 if (_freeHandles.Count <= 0) return;
-                var handle = _freeHandles.Dequeue(); // TODO pick nearest 
+                var handle = PickHandleForFinger(finger);  
                 handle.finger = finger;
                 _pairings.Add(finger, handle);
             }
+        }
+
+        private Handle PickHandleForFinger(Finger finger)
+        {
+            var nearestHandleIndex = 0;
+            if (_freeHandles.Count > 1)
+            {
+                var fingerPos = _camera.ScreenToWorldPoint(finger.screenPosition);
+                var minDistance = float.MaxValue;
+                for (int i = 0; i < _freeHandles.Count; i++)
+                {
+                    var curDistance = Vector2.Distance(fingerPos, _freeHandles[i].transform.position); 
+                    if (curDistance < minDistance)
+                    {
+                        minDistance = curDistance;
+                        nearestHandleIndex = i;
+                    }
+                }
+            }
+            var handle = _freeHandles[nearestHandleIndex];
+            _freeHandles.Remove(handle);
+            return handle;
         }
 
         private void OnFingerUp(Finger finger)
@@ -61,7 +85,7 @@ namespace FingerFighter.Control.Character.Handles
                 var handle = _pairings[finger];
                 handle.finger = null;
                 _pairings.Remove(finger);
-                _freeHandles.Enqueue(handle);
+                _freeHandles.Add(handle);
             }
         }
 
