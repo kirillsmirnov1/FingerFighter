038ed8b684113bf2597a1f55681e07d8d1c3065e
Kirill Smirnov <thegaldren@gmail.com>
2021-10-22 13:59:00 +0300

    CombatPointsDisplay.cs un Runner.unity

---
+++ Assets/FingerFighter/Code/Model/Combat/PointsCounter.cs
@@ -0,0 +1,26 @@
+﻿using FingerFighter.Control.Combat;
+using UnityEngine;
+using UnityUtils.Variables;
+
+namespace FingerFighter.Model.Combat
+{
+    public class PointsCounter : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable pointsCounter;
+        [SerializeField] private EnemyStatsList enemyData;
+        
+        private void Awake()
+        {
+            pointsCounter.Value = 0;
+            EnemyStatus.OnDeath += OnEnemyDeath;
+        }
+
+        private void OnDestroy() => EnemyStatus.OnDeath -= OnEnemyDeath;
+
+        private void OnEnemyDeath(string enemyTag, Vector2 pos)
+        {
+            var points = enemyData[enemyTag].health; // IMPR might change points logic later 
+            pointsCounter.Value += points;
+        }
+    }
+}
