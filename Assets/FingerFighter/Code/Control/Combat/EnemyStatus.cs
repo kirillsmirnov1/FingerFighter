abe4767cc93e21b6c21b5c075309a72f49518ea3
Kirill Smirnov <thegaldren@gmail.com>
2021-10-22 13:30:36 +0300

    EnemyStatus.cs notifies on EnemyDeath

--- Assets/FingerFighter/Code/Control/Combat/EnemyStatus.cs
+++ Assets/FingerFighter/Code/Control/Combat/EnemyStatus.cs
@@ -1,12 +1,20 @@
-﻿using FingerFighter.Control.Factories.EnemySpawn;
+﻿using System;
+using FingerFighter.Control.Factories.EnemySpawn;
+using UnityEngine;
 
 namespace FingerFighter.Control.Combat
 {
     public class EnemyStatus : CombatEntityStatus
     {
+        /// <summary>
+        /// tag and position
+        /// </summary>
+        public static event Action<string, Vector2> OnDeath;
+        
         protected override void OnDisable()
         {
             base.OnDisable();
+            OnDeath?.Invoke(id.EnemyType, transform.position);
             AEnemySpawn.ReturnToPool(gameObject, id.EnemyType);
         }
     }
