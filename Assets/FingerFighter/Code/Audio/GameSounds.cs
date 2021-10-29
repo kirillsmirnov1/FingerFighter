b59b26289cdf22cde29a34e1e8ccb4debe35dd16
Kirill Smirnov <thegaldren@gmail.com>
2021-10-29 20:03:47 +0300

    GameSounds.cs hits

---
+++ Assets/FingerFighter/Code/Audio/GameSounds.cs
@@ -0,0 +1,34 @@
+﻿using FingerFighter.Control.Damage;
+using FingerFighter.Model.Damage;
+using UnityEngine;
+
+namespace FingerFighter.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class GameSounds : MonoBehaviour
+    {
+        [SerializeField] private AudioClip[] enemyHitSounds;
+        // TODO enemyDeathSounds
+        // TODO playerDeath sound 
+        
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+
+            HitTaker.OnHitTaken += OnHitTaken;
+        }
+
+        private void OnDestroy()
+        {
+            HitTaker.OnHitTaken -= OnHitTaken;
+        }
+
+        private void OnHitTaken(HitData obj)
+        {
+            // TODO another sound for player hit 
+            _audioSource.PlayOneShot(enemyHitSounds[Random.Range(0, enemyHitSounds.Length)]);
+        }
+    }
+}
