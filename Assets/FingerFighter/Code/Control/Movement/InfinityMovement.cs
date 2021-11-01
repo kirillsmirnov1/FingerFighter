fa1841d5acf03896f2b38b7ba43a1dd685904453
Kirill Smirnov <thegaldren@gmail.com>
2021-11-01 16:08:03 +0300

    Add Zoo.unity for enemy testing

---
+++ Assets/FingerFighter/Code/Control/Movement/InfinityMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.Control.Movement
+{
+    public class InfinityMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 1f;
+        [SerializeField] private float radius = 1f;
+        
+        private float _t;
+
+        private void Update()
+        {
+            _t += Time.deltaTime;
+            var t = speed * _t;
+            transform.position = radius 
+                * new Vector2(Mathf.Sin(2 * t) / 2, Mathf.Cos(t));
+        }
+    }
+}
