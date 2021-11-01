87f3f6dbc46b596f11cd91b5a1368add21f63ab3
Kirill Smirnov <thegaldren@gmail.com>
2021-11-01 16:37:14 +0300

    SnakeBody.cs and SnakeSegment.cs prototyping

---
+++ Assets/FingerFighter/Code/Control/Enemies/Snake/SnakeBody.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace FingerFighter.Control.Enemies.Snake
+{
+    public class SnakeBody : MonoBehaviour
+    {
+        [SerializeField] private TransformVariable player;
+        public Transform Player => player.Value;
+    }
+}
---
+++ Assets/FingerFighter/Code/Control/Enemies/Snake/SnakeSegment.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.Control.Enemies.Snake
+{
+    public class SnakeSegment : MonoBehaviour
+    {
+        [SerializeField] private SnakeBody body;
+        
+        [SerializeField] private SnakeSegment previous;
+        [SerializeField] private SnakeSegment next;
+
+        private SnakeSegmentState _state;
+        
+        private void OnEnable()
+        {
+            SetState();
+        }
+
+        private void SetState()
+        {
+            _state = previous == null ? (SnakeSegmentState) new Head() : new Segment();
+        }
+
+        private void FixedUpdate()
+        {
+            _state.FixedUpdate();
+        }
+
+        private abstract class SnakeSegmentState
+        {
+            public abstract void FixedUpdate();
+        }
+
+        private class Head : SnakeSegmentState
+        {
+            public override void FixedUpdate()
+            {
+                // TODO follow player 
+            }
+        }
+
+        private class Segment : SnakeSegmentState
+        {
+            public override void FixedUpdate()
+            {
+                // TODO follow previous segment 
+            }
+        }
+    }
+}
