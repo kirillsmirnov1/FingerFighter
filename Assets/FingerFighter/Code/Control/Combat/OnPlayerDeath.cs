faef768372e40ad69b79b86d85265a39174e4b86
Kirill Smirnov <thegaldren@gmail.com>
2021-11-11 15:15:03 +0300

    OnPlayerDeath.cs updates highscore

--- Assets/FingerFighter/Code/Control/Combat/OnPlayerDeath.cs
+++ Assets/FingerFighter/Code/Control/Combat/OnPlayerDeath.cs
@@ -1,14 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityUtils.Variables;
 
 namespace FingerFighter.Control.Combat
 {
     public class OnPlayerDeath : MonoBehaviour
     {
         [SerializeField] private UnityEvent actions;
+        [SerializeField] private FloatVariable runScore;
+        [SerializeField] private LongVariable runHighScore;
         
         private void Awake() => PlayerStatus.OnDeath += OnDeath;
         private void OnDestroy() => PlayerStatus.OnDeath -= OnDeath;
-        private void OnDeath() => actions.Invoke();
+        private void OnDeath()
+        {
+            var newHighScore = runScore.Value > runHighScore.Value;
+            if (newHighScore) runHighScore.Value = (long) runScore.Value;
+            // TODO hide overlay score 
+            // TODO if high score, show prompt 
+
+            // TODO show view 
+            actions.Invoke();
+        }
     }
 }
