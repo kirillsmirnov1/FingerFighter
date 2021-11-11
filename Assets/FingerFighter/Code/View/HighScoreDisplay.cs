3bcb99b0ef4066e3180e873ae0e04db5cfcf1436
Kirill Smirnov <thegaldren@gmail.com>
2021-11-11 16:00:58 +0300

    Ring has HighScoreDisplay.cs

---
+++ Assets/FingerFighter/Code/View/HighScoreDisplay.cs
@@ -0,0 +1,13 @@
+﻿using UnityUtils.Variables.Display;
+
+namespace FingerFighter.View
+{
+    public class HighScoreDisplay : LongVariableDisplay
+    {
+        protected override void SetText(long value)
+        {
+            Text.gameObject.SetActive(value > 0);
+            base.SetText(value);
+        }
+    }
+}
