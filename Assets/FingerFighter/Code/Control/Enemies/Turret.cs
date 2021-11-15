e5a81a43071c7c7612470c967335aac8d42b25e3
Kirill Smirnov <thegaldren@gmail.com>
2021-11-15 13:00:47 +0300

    Add Turret.cs to Spikes.prefab

---
+++ Assets/FingerFighter/Code/Control/Enemies/Turret.cs
@@ -0,0 +1,42 @@
+﻿using FingerFighter.Control.Factories;
+using FingerFighter.Model.Combat;
+using FingerFighter.Model.Util;
+using UnityEngine;
+
+namespace FingerFighter.Control.Enemies
+{
+    public class Turret : MonoBehaviour
+    {
+        [SerializeField] private float shotDelay = .3f;
+        [SerializeField] private float afterShotRotation = 10;
+        [SerializeField] private EnemyStats projectileType;
+        [SerializeField] private float projectileImpulse = 10;
+        
+        private float _rotation;
+        private float _timeForANextShot;
+
+        private void Update()
+        {
+            if (Time.time > _timeForANextShot)
+            {
+                MakeAShot();
+                IncrementParams();
+            }
+        }
+
+        private void IncrementParams()
+        {
+            _timeForANextShot = Time.time + shotDelay;
+            _rotation = (_rotation + afterShotRotation) % 360f;
+        }
+
+        private void MakeAShot()
+        {
+            var newProjectile = EnemyPool.Get(projectileType.tag).GetComponent<Rigidbody2D>();
+            
+            newProjectile.position = transform.position;
+            newProjectile.rotation = _rotation;
+            newProjectile.AddForce(projectileImpulse * Vector2Ext.DegreeToVector2(_rotation), ForceMode2D.Impulse);
+        }
+    }
+}
