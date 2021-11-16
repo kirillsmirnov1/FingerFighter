0c65a8b620b395cd7125d307150bd43028ca2cf6
Kirill Smirnov <thegaldren@gmail.com>
2021-11-16 16:59:36 +0300

    Add EnemyFormationPackArray.cs

---
+++ Assets/FingerFighter/Code/Model/EnemyFormations/EnemyFormationPackArray.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace FingerFighter.Model.EnemyFormations
+{
+    [CreateAssetMenu(menuName = "Data/EnemyFormationPackArray", fileName = "EnemyFormationPack", order = 0)]
+    public class EnemyFormationPackArray : XArrayVariable<EnemyFormationPack>
+    {
+        
+    }
+}
