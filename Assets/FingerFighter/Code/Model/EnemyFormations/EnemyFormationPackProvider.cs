52ae0a8faecc58c49b8c7d30937ea7eddff437f7
Kirill Smirnov <thegaldren@gmail.com>
2021-11-16 17:29:46 +0300

    EnemyFormationPackProvider.cs returns part of formations and a boss

--- Assets/FingerFighter/Code/Model/EnemyFormations/EnemyFormationPackProvider.cs
+++ Assets/FingerFighter/Code/Model/EnemyFormations/EnemyFormationPackProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityUtils;
 
@@ -10,12 +11,34 @@ namespace FingerFighter.Model.EnemyFormations
         [SerializeField] private EnemyFormationPackArray packs;
         [SerializeField] private int formationsPerRun = 10;
 
-        public EnemyFormation[] NextPack()
+        public IEnumerable<EnemyFormation> NextPack()
         {
             var pack = PickAPack();
-            // TODO pick formations
-            // TODO add boss 
-            return pack.Formations; 
+            var formations = PickFormations(pack.Formations);
+            return formations
+                .Append(BossEntry(pack));
+        }
+
+        private static EnemyFormation BossEntry(EnemyFormationPack pack) 
+            => new EnemyFormation {
+                entries = new [] {
+                    new EnemyFormationEntry {
+                        enemy = pack.Boss,
+                        pos = Vector2.zero,
+                    }}};
+
+        private IEnumerable<EnemyFormation> PickFormations(EnemyFormation[] formations)
+        {
+            var indexes = Enumerable
+                .Range(0, formations.Length)
+                .Shuffle()
+                .Take(formationsPerRun)
+                .OrderBy(x => x);
+            
+            foreach (var index in indexes)
+            {
+                yield return formations[index];
+            }
         }
 
         private EnemyFormationPack PickAPack() 
