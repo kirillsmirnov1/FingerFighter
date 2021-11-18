237022f68afe7090af7701c3ac0b0b783b133232
Kirill Smirnov <thegaldren@gmail.com>
2021-11-18 11:56:28 +0300

    Start MultiTailHead.cs

---
+++ Assets/FingerFighter/Code/Control/Enemies/Snake/MultiTailHead.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace FingerFighter.Control.Enemies.Snake
+{
+    public class MultiTailHead : MonoBehaviour
+    {
+        // TODO lock health until tails are dead
+        // TODO position subheads on enable
+        // TODO enable head segment onEnable
+        // TODO move head segment towards player 
+    }
+}
