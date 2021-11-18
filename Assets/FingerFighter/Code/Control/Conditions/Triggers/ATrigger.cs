2dc57910854a7f1d26d6163365affd3268d0a438
Kirill Smirnov <thegaldren@gmail.com>
2021-11-18 13:48:22 +0300

    ATrigger.cs

---
+++ Assets/FingerFighter/Code/Control/Conditions/Triggers/ATrigger.cs
@@ -0,0 +1,23 @@
+﻿using FingerFighter.Control.Conditions.Flags;
+using UnityEngine;
+
+namespace FingerFighter.Control.Conditions.Triggers
+{
+    public abstract class ATrigger : MonoBehaviour
+    {
+        [SerializeField] private AFlag flag;
+
+        private void OnEnable()
+        {
+            InvokeTrigger(flag.On);
+            flag.OnChange += InvokeTrigger;
+        }
+
+        private void OnDisable()
+        {
+            flag.OnChange -= InvokeTrigger;
+        }
+
+        protected abstract void InvokeTrigger(bool flagIsOn);
+    }
+}
