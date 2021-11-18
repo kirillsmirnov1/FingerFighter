a0c73d8866b1f0ffe32cf73b87762cdd2df7e8e5
Kirill Smirnov <thegaldren@gmail.com>
2021-11-18 13:48:14 +0300

    AFlag.OnChange()

--- Assets/FingerFighter/Code/Control/Conditions/Flags/AFlag.cs
+++ Assets/FingerFighter/Code/Control/Conditions/Flags/AFlag.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace FingerFighter.Control.Conditions.Flags
 {
     public abstract class AFlag : MonoBehaviour
     {
-        public bool On { get; protected set; }
+        public event Action<bool> OnChange; 
+        private bool _on;
+
+        public bool On
+        {
+            get => _on;
+            protected set
+            {
+                if(value == _on) return;
+                _on = value;
+                OnChange?.Invoke(_on);
+            }
+        }
     }
 }
