ffa80d11e47b8ae6121bf0226badb113278fea22
Kirill Smirnov <thegaldren@gmail.com>
2021-11-18 12:01:03 +0300

    Enable multiheaded snake head on enable

--- Assets/FingerFighter/Code/Control/Enemies/Snake/MultiTailHead.cs
+++ Assets/FingerFighter/Code/Control/Enemies/Snake/MultiTailHead.cs
@@ -4,9 +4,15 @@ namespace FingerFighter.Control.Enemies.Snake
 {
     public class MultiTailHead : MonoBehaviour
     {
+        [SerializeField] private GameObject headSegment;
+
         // TODO lock health until tails are dead
         // TODO position subheads on enable
-        // TODO enable head segment onEnable
         // TODO move head segment towards player 
+
+        private void OnEnable()
+        {
+            headSegment.gameObject.SetActive(true);
+        }
     }
 }
