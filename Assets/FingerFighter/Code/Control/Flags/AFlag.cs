f527e46e90c52f83638ca25ced3ba0ab1487dfd5
Kirill Smirnov <thegaldren@gmail.com>
2021-11-18 13:23:26 +0300

    Add AFlag.cs and StageHealthFlag.cs

---
+++ Assets/FingerFighter/Code/Control/Flags/AFlag.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.Control.Flags
+{
+    public abstract class AFlag : MonoBehaviour
+    {
+        public bool On { get; protected set; }
+    }
+}
---
+++ Assets/FingerFighter/Code/Control/Flags/StageHealthFlag.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using FingerFighter.Control.Combat.Health;
+using UnityEngine;
+
+namespace FingerFighter.Control.Flags
+{
+    /// <summary>
+    /// On while all stageHealths are on 
+    /// </summary>
+    public class StageHealthFlag : AFlag
+    {
+        [SerializeField] private AHealth[] stageHealths;
+
+        private void OnEnable()
+        {
+            for (int i = 0; i < stageHealths.Length; i++)
+            {
+                stageHealths[i].onNoHealth += CheckStageHealthsOnDepletion;
+            }
+        }
+
+        private void OnDisable()
+        {
+            for (int i = 0; i < stageHealths.Length; i++)
+            {
+                stageHealths[i].onNoHealth -= CheckStageHealthsOnDepletion;
+            }
+        }
+
+        private void CheckStageHealthsOnDepletion()
+        {
+            On = stageHealths.Any(h => !h.NoHealth);
+        }
+        
+    }
+}
