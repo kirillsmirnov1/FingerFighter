2fcd6144e2998508c1465d6b012f9423b59cf19b
Kirill Smirnov <thegaldren@gmail.com>
2021-11-23 14:06:28 +0300

    Show score on OnPlayerDeathCanvas.prefab even if it is zero

--- Assets/FingerFighter/Code/View/Ui/CombatPointsDisplay.cs
+++ Assets/FingerFighter/Code/View/Ui/CombatPointsDisplay.cs
@@ -1,12 +1,15 @@
-﻿using UnityUtils.Variables.Display;
+﻿using UnityEngine;
+using UnityUtils.Variables.Display;
 
 namespace FingerFighter.View.Ui
 {
     public class CombatPointsDisplay : FloatVariableDisplay
     {
+        [SerializeField] private bool showZero;
+        
         protected override void SetText(float value)
         {
-            Text.gameObject.SetActive(value >= 1f);
+            Text.gameObject.SetActive(value >= 1f || showZero);
             Text.text = $"{prefix}{value:0}{postfix}";
         }
     }
