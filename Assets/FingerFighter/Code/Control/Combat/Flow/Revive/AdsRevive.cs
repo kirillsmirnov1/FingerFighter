e5a078d0156d220b5841bb9f33ddb2380d6d1195
Kirill Smirnov <thegaldren@gmail.com>
2021-11-29 17:40:20 +0300

    AdsRevive.cs

---
+++ Assets/FingerFighter/Code/Control/Combat/Flow/Revive/AdsRevive.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityUtils.Events;
+
+namespace FingerFighter.Control.Combat.Flow.Revive
+{
+    public class AdsRevive : MonoBehaviour, IUnityAdsShowListener
+    {
+        [SerializeField] private GameEvent request;
+        [SerializeField] private GameEvent reward;
+
+        private string _adUnitId;
+        
+        private void Awake()
+        {
+            _adUnitId = Application.platform == RuntimePlatform.IPhonePlayer
+                ? "Rewarded_iOS"
+                : "Rewarded_Android";
+            LoadAd();
+            request.RegisterAction(OnReviveRequest);
+        }
+
+        private void OnDestroy()
+        {
+            request.UnregisterAction(OnReviveRequest);
+        }
+
+        private void LoadAd()
+        {
+            Advertisement.Load(_adUnitId);
+        }
+
+        private void OnReviveRequest()
+        {
+            ShowAd();
+        }
+
+        private void ShowAd()
+        {
+            Advertisement.Show(_adUnitId, this);
+        }
+
+        public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+        {
+            Debug.Log("UnityAdsShowComplete"); // TODO find a way to test in editor 
+            reward.Raise();
+            LoadAd();
+        }
+
+        public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+        {
+            Debug.LogWarning("UnityAdsShowFailure");
+        }
+
+        public void OnUnityAdsShowStart(string placementId)
+        {
+            Debug.LogWarning("UnityAdsShowStart");
+        }
+
+        public void OnUnityAdsShowClick(string placementId)
+        {
+            Debug.LogWarning("UnityAdsShowClick");
+        }
+    }
+}
