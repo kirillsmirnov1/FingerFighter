8fa840cda089c3001fd72c472ac8558ebde5645b
Kirill Smirnov <thegaldren@gmail.com>
2021-11-30 16:59:11 +0300

    GiveCoinsOnEnemyDeath.cs

---
+++ Assets/FingerFighter/Code/Control/Balance/GiveCoinsOnEnemyDeath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FingerFighter.Control.Combat.Status;
+using FingerFighter.Model.Combat;
+using UnityEngine;
+using UnityUtils.Variables;
+
+namespace FingerFighter.Control.Balance
+{
+    public class GiveCoinsOnEnemyDeath : MonoBehaviour
+    {
+        [SerializeField] private EnemyStatsList stats;
+        
+        [SerializeField] private ULongVariable coinBalance;
+
+        private void Awake()
+        {
+            EnemyStatus.OnDeath += GiveCoins;
+        }
+
+        private void OnDestroy()
+        {
+            EnemyStatus.OnDeath -= GiveCoins;
+        }
+
+        private void GiveCoins(EnemyDeathData edd)
+        {
+            coinBalance.Value += (ulong) stats[edd.Tag].coins;
+        }
+    }
+}
