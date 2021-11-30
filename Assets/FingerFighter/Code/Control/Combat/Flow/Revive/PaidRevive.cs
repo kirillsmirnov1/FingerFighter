2f37ea574a85bd5f88690d40f9864e3e06943676
Kirill Smirnov <thegaldren@gmail.com>
2021-11-30 18:16:52 +0300

    PaidRevive.cs

---
+++ Assets/FingerFighter/Code/Control/Combat/Flow/Revive/PaidRevive.cs
@@ -0,0 +1,50 @@
+﻿using FingerFighter.Control.Combat.Status;
+using FingerFighter.View.Display;
+using UnityEngine;
+using UnityUtils.Events;
+using UnityUtils.Variables;
+
+namespace FingerFighter.Control.Combat.Flow.Revive
+{
+    public class PaidRevive : MonoBehaviour
+    {
+        [SerializeField] private ulong baseReviveCost = 500;
+        [SerializeField] private ULongVariable reviveCostVariable;
+        [SerializeField] private ULongVariable coinBalance;
+        [SerializeField] private GameEvent paidReviveRequest;
+        [SerializeField] private GameEvent reward;
+        
+        private uint _costMultiplier;
+
+        private void Awake()
+        {
+            PlayerStatus.OnAlive += IterateReviveCostMultiplier;
+            paidReviveRequest.RegisterAction(TryPaidRevive);
+        }
+
+        private void OnDestroy()
+        {
+            PlayerStatus.OnAlive -= IterateReviveCostMultiplier;
+            paidReviveRequest.UnregisterAction(TryPaidRevive);
+        }
+
+        private void IterateReviveCostMultiplier()
+        {
+            _costMultiplier++;
+            reviveCostVariable.Value = baseReviveCost * _costMultiplier;
+        }
+
+        private void TryPaidRevive()
+        {
+            if (coinBalance.Value >= reviveCostVariable.Value)
+            {
+                coinBalance.Value -= reviveCostVariable.Value;
+                reward.Raise();
+            }
+            else
+            {
+                UiNotifications.Show("Don't have enough coins to revive.");
+            }
+        }
+    }
+}
