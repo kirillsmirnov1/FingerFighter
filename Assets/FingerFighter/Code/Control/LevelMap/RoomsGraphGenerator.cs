49e64220d72968d67b8ebd9c8aa177dcb0d5f413
Kirill Smirnov <thegaldren@gmail.com>
2021-12-01 02:10:05 +0300

    RoomsGraphGenerator.cs generate rooms

--- Assets/FingerFighter/Code/Control/LevelMap/RoomsGraphGenerator.cs
+++ Assets/FingerFighter/Code/Control/LevelMap/RoomsGraphGenerator.cs
@@ -1,18 +1,27 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 namespace FingerFighter.Control.LevelMap
 {
     public class RoomsGraphGenerator : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField] private Vector2Int roomCount = new Vector2Int(6, 10);
+        [SerializeField] private Vector2Int minPos = new Vector2Int(-2, -3);
+        [SerializeField] private Vector2Int maxPos = new Vector2Int(2, 3);
+        
+        [Header("Results")]
         [SerializeField] private List<Room> rooms;
+
+        private static readonly Random Rand = new Random();
         
         private void OnDrawGizmos()
         {
             foreach (var room in rooms)
             {
-                Gizmos.DrawSphere((Vector3Int) room.gridPos, 0.1f);   
+                Gizmos.DrawSphere(room.pos, 0.1f);   
             }
         }
 
@@ -20,17 +29,30 @@ namespace FingerFighter.Control.LevelMap
         {
             rooms = new List<Room>
             {
-                new Room {gridPos = new Vector2Int(0, -4)}, // Starter 
-                new Room {gridPos = new Vector2Int(0, 4)} // Boss
+                new Room {pos = new Vector2(0, -4)}, // Starter 
+                new Room {pos = new Vector2(0, 4)} // Boss
             };
             
-            // TODO add more
+            var roomsToAdd = Rand.Next(roomCount.x, roomCount.y + 1);
+            var newRoom = rooms[0];
+            for (var i = 0; i < roomsToAdd; i++)
+            {
+                while (rooms.Contains(newRoom))
+                {
+                    newRoom = new Room {
+                        pos = new Vector2(
+                            Rand.Next(minPos.x, maxPos.x + 1),
+                            Rand.Next(minPos.y, maxPos.y + 1)) // TODO randomize 
+                    };
+                } 
+                rooms.Add(newRoom);
+            }
         }
     }
 
     [Serializable]
     public struct Room
     {
-        public Vector2Int gridPos;
+        public Vector2 pos;
     }
 }
