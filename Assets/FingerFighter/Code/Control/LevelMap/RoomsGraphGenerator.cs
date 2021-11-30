69d4aee279a0dbd6c32d3059b965b6996c903c5f
Kirill Smirnov <thegaldren@gmail.com>
2021-12-01 02:52:32 +0300

    RoomsGraphGenerator.cs sets shift to rooms

--- Assets/FingerFighter/Code/Control/LevelMap/RoomsGraphGenerator.cs
+++ Assets/FingerFighter/Code/Control/LevelMap/RoomsGraphGenerator.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityUtils;
 using Random = System.Random;
 
 namespace FingerFighter.Control.LevelMap
@@ -12,6 +14,7 @@ namespace FingerFighter.Control.LevelMap
         [SerializeField] private Vector2Int minPos = new Vector2Int(-2, -3);
         [SerializeField] private Vector2Int maxPos = new Vector2Int(2, 3);
         [SerializeField] private float roomMarkRadius = 0.2f;
+        [SerializeField] private float shiftMaxRadius = 0.25f;
         
         [Header("Results")]
         [SerializeField] private List<Room> rooms;
@@ -22,38 +25,62 @@ namespace FingerFighter.Control.LevelMap
         {
             foreach (var room in rooms)
             {
+                // Gizmos.DrawSphere((Vector3Int)room.gridPos, roomMarkRadius);   
                 Gizmos.DrawSphere(room.pos, roomMarkRadius);   
             }
         }
 
         public void Generate()
         {
+            SetAnchorRooms();
+            GenerateMiddleRooms();
+        }
+
+        private void SetAnchorRooms()
+        {
+            var top = new Vector2Int(0, 4);
             rooms = new List<Room>
             {
-                new Room {pos = new Vector2(0, -4)}, // Starter 
-                new Room {pos = new Vector2(0, 4)} // Boss
+                new Room {gridPos = -top, pos = -top}, // Start
+                new Room {gridPos = top, pos = top} // End
             };
-            
+        }
+
+        private void GenerateMiddleRooms()
+        {
             var roomsToAdd = Rand.Next(roomCount.x, roomCount.y + 1);
-            var newRoom = rooms[0];
+            var positions = new HashSet<Vector2Int>(rooms.Select(r => r.gridPos));
+            var newPos = rooms[0].gridPos;
+
             for (var i = 0; i < roomsToAdd; i++)
             {
-                while (rooms.Contains(newRoom))
+                while (positions.Contains(newPos))
                 {
-                    newRoom = new Room {
-                        pos = new Vector2(
-                            Rand.Next(minPos.x, maxPos.x + 1),
-                            Rand.Next(minPos.y, maxPos.y + 1)) // TODO randomize 
-                    };
-                } 
-                rooms.Add(newRoom);
+                    newPos = new Vector2Int(
+                        Rand.Next(minPos.x, maxPos.x + 1),
+                        Rand.Next(minPos.y, maxPos.y + 1));
+                }
+                positions.Add(newPos);
+
+                rooms.Add(new Room
+                {
+                    gridPos = newPos,
+                    pos = newPos + NextShift,
+                });
             }
         }
+
+        private Vector2 NextShift =>
+            new Vector2(
+                Rand.NextFloat(-shiftMaxRadius, shiftMaxRadius),
+                Rand.NextFloat(-shiftMaxRadius, shiftMaxRadius)
+            );
     }
 
     [Serializable]
-    public struct Room
+    public class Room
     {
+        public Vector2Int gridPos;
         public Vector2 pos;
     }
 }
