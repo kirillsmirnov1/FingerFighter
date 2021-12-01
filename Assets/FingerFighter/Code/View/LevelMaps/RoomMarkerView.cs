7b47e0e0e4d5631b7f7046d6fb64ff74ee4d15fe
Kirill Smirnov <thegaldren@gmail.com>
2021-12-01 05:37:36 +0300

    RoomMarkerView.cs notifies on click

--- Assets/FingerFighter/Code/View/LevelMaps/RoomMarkerView.cs
+++ Assets/FingerFighter/Code/View/LevelMaps/RoomMarkerView.cs
@@ -1,21 +1,42 @@
-﻿using FingerFighter.Model.LevelMaps;
+﻿using System;
+using FingerFighter.Model.LevelMaps;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace FingerFighter.View.LevelMaps
 {
+    [RequireComponent(typeof(Button))]
     [RequireComponent(typeof(RectTransform))]
     public class RoomMarkerView : MonoBehaviour
     {
+        public static event Action<int> OnClick; 
+
         [SerializeField] private RectTransform rect;
+        [SerializeField] private Button button;
 
         private RoomMarkerData _data;
         
         // TODO set button interaction depending on proximity to player
-        // TODO notify on click
-        
+
         private void OnValidate()
         {
             rect ??= GetComponent<RectTransform>();
+            button ??= GetComponent<Button>();
+        }
+
+        private void Awake()
+        {
+            button.onClick.AddListener(NotifyOnClick);
+        }
+
+        private void OnDestroy()
+        {
+            button.onClick.RemoveListener(NotifyOnClick);
+        }
+
+        private void NotifyOnClick()
+        {
+            OnClick?.Invoke(_data.roomIndex);
         }
 
         public void Init(RoomMarkerData data)
