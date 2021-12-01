0c0be1e9bf64d84654d0380c531011422a565a44
Kirill Smirnov <thegaldren@gmail.com>
2021-12-01 04:23:44 +0300

    LevelMapDisplay.cs stub

---
+++ Assets/FingerFighter/Code/Editor/View/LevelMaps/LevelMapsDisplayInspector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace FingerFighter.View.LevelMaps
+{
+    [CustomEditor(typeof(LevelMapDisplay))]
+    public class LevelMapsDisplayInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Spawn Map"))
+            {
+                (target as LevelMapDisplay).SpawnMap();
+            }
+            if (GUILayout.Button("Clear Map"))
+            {
+                (target as LevelMapDisplay).ClearMap();
+            }
+        }
+    }
+}
---
+++ Assets/FingerFighter/Code/View/LevelMaps/LevelMapDisplay.cs
@@ -0,0 +1,30 @@
+﻿using FingerFighter.Model.LevelMaps;
+using UnityEngine;
+
+namespace FingerFighter.View.LevelMaps
+{
+    public class LevelMapDisplay : MonoBehaviour
+    {
+        [SerializeField] private LevelMapVariable levelMapVariable;
+        [SerializeField] private GameObject roomMarkPrefab;
+        [SerializeField] private GameObject connectionPrefab;
+
+        private void Awake()
+        {
+            SpawnMap();
+        }
+
+        public void SpawnMap()
+        {
+            Debug.Log("TODO spawn map");
+            ClearMap();
+            // TODO spawn marks
+            // TODO spawn connections 
+        }
+
+        public void ClearMap()
+        {
+            Debug.Log("TODO clean map");
+        }
+    }
+}
