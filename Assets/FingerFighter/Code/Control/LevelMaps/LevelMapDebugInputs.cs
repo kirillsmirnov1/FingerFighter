0716afa84d338dab62f8084e21a29295b90e7e55
Kirill Smirnov <thegaldren@gmail.com>
2021-12-02 19:32:46 +0300

    LevelMapDebugInputs.cs

---
+++ Assets/FingerFighter/Code/Control/LevelMaps/LevelMapDebugInputs.cs
@@ -0,0 +1,23 @@
+﻿using FingerFighter.View.LevelMaps;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FingerFighter.Control.LevelMaps
+{
+    public class LevelMapDebugInputs : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [SerializeField] private RoomsGraphGenerator levelMapGenerator;
+        [SerializeField] private LevelMapDisplay levelMapDisplay;
+
+        private void Update()
+        {
+            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                levelMapGenerator.Generate();
+                levelMapDisplay.SpawnMap();
+            }
+        }
+#endif
+    }
+}
