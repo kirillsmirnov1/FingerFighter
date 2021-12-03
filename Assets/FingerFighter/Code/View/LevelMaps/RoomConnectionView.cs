9304b90a2d9a0ef2752bf7f0b0ac27426c3fd91d
Kirill Smirnov <thegaldren@gmail.com>
2021-12-03 16:13:41 +0300

    RoomConnectionView.cs sets material

--- Assets/FingerFighter/Code/View/LevelMaps/RoomConnectionView.cs
+++ Assets/FingerFighter/Code/View/LevelMaps/RoomConnectionView.cs
@@ -1,5 +1,8 @@
 ﻿using FingerFighter.Model.LevelMaps;
+using FingerFighter.View.LevelMaps.Player;
 using UnityEngine;
+using UnityUtils;
+using UnityUtils.Variables;
 
 namespace FingerFighter.View.LevelMaps
 {
@@ -7,20 +10,40 @@ namespace FingerFighter.View.LevelMaps
     public class RoomConnectionView : MonoBehaviour
     {
         [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private IntVariable currentRoom;
+        [SerializeField] private Material availableMat;
+        [SerializeField] private Material unAvailableMat;
         
         private RoomConnectionData _data;
         private Vector2Int Connection => _data.connection;
-        // TODO set line sprite depending on proximity to player's mark
 
         private void OnValidate()
         {
             lineRenderer ??= GetComponent<LineRenderer>();
+            this.CheckNullFields();
+        }
+
+        private void Awake()
+        {
+            PlayerMarker.OnRoomReached += SetLineMaterial;
+        }
+
+        private void OnDestroy()
+        {
+            PlayerMarker.OnRoomReached -= SetLineMaterial;
         }
 
         public void Init(RoomConnectionData data)
         {
             _data = data;
             lineRenderer.SetPositions(_data.positions);
+            SetLineMaterial(currentRoom);
+        }
+
+        private void SetLineMaterial(int currentRoomIndex)
+        {
+            var availableConnection = currentRoomIndex == Connection.x || currentRoomIndex == Connection.y;
+            lineRenderer.material = availableConnection ? availableMat : unAvailableMat;
         }
     }
 }
