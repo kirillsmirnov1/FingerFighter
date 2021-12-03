1596ef96d3df76dd495823cd92338c49225c6680
Kirill Smirnov <thegaldren@gmail.com>
2021-12-03 14:54:21 +0300

    Start LevelMapFlow.cs

---
+++ Assets/FingerFighter/Code/Control/LevelMaps/LevelMapFlow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.Control.LevelMaps
+{
+    public class LevelMapFlow : MonoBehaviour
+    {
+        // TODO on player on room — check it, start it
+        // TODO on boss defeat — move next 
+    }
+}
