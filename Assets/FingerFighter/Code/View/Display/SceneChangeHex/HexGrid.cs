5017584b0c126d0a4b9037f102e0853c20012a84
Kirill Smirnov <thegaldren@gmail.com>
2021-12-08 09:55:11 +0300

    Optimize HexGrid scaling

--- Assets/FingerFighter/Code/View/Display/SceneChangeHex/HexGrid.cs
+++ Assets/FingerFighter/Code/View/Display/SceneChangeHex/HexGrid.cs
@@ -5,23 +5,38 @@ namespace FingerFighter.View.Display.SceneChangeHex
     public class HexGrid : MonoBehaviour
     {
         [SerializeField][Range(0f, 1f)] private float t;
+        [Tooltip("Gap between rows scaling")]
         [SerializeField] private float delay = 0.01f;
-        [SerializeField] private float k = 5;
-        [SerializeField] private float totalDelay;
-        [SerializeField] private HexRow[] rows;
+        [Tooltip("Relative time it takes for row to scale")]
+        [SerializeField] private float duration = 0.1f;
+        
+        [HideInInspector][SerializeField] private float tMod;
+        [HideInInspector][SerializeField] private HexRow[] rows;
+        [HideInInspector][SerializeField] private float[] rowsDelay;
         
         private void OnValidate()
         {
             rows = GetComponentsInChildren<HexRow>();
-            totalDelay = delay * (rows.Length + k);
+            tMod = rows.Length * delay + duration;
+            InitRowsDelay();
             Scale(t);
+        }
+
+        private void InitRowsDelay()
+        {
+            rowsDelay = new float[rows.Length];
+            for (int i = 0; i < rowsDelay.Length; i++)
+            {
+                rowsDelay[i] = i * delay;
+            }
         }
 
         public void Scale(float scale)
         {
             for (int i = 0; i < rows.Length; i++)
             {
-                rows[i].Scale(Mathf.Clamp01(scale * totalDelay - delay * (rows.Length - i - 1)));
+                var index = rows.Length - 1 - i;
+                rows[index].Scale(Mathf.InverseLerp(rowsDelay[i], rowsDelay[i] + duration, scale * tMod));
             }
         }
     }
