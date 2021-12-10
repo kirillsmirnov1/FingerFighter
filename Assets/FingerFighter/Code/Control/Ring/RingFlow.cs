8532b2d5b4ac425080e4d00ddc548f8c47fe7d75
Kirill Smirnov <thegaldren@gmail.com>
2021-12-10 12:47:10 +0300

    RingFlow.cs resets player health

--- Assets/FingerFighter/Code/Control/Ring/RingFlow.cs
+++ Assets/FingerFighter/Code/Control/Ring/RingFlow.cs
@@ -1,4 +1,5 @@
-﻿using FingerFighter.Model.LevelMaps;
+﻿using FingerFighter.Control.Combat.Health;
+using FingerFighter.Model.LevelMaps;
 using UnityEngine;
 using UnityUtils.Variables;
 
@@ -8,11 +9,13 @@ namespace FingerFighter.Control.Ring
     {
         [SerializeField] private LevelMapVariable levelMapVariable;
         [SerializeField] private FloatVariable levelScore;
+        [SerializeField] private PlayerHealthHelper playerHealthHelper;
         
         private void Awake()
         {
             levelMapVariable.Value = null; // Because ring loaded only when level is over
             levelScore.Value = 0;
+            playerHealthHelper.Revive();
         }
     }
 }
