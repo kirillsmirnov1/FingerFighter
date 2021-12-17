dceef2d1d806a4b2ada0b0e6520fb2f91e13a563
Kirill Smirnov <thegaldren@gmail.com>
2021-12-17 13:16:20 +0300

    Try to use sprite as shield

---
+++ Assets/FingerFighter/Code/Control/Common/Conditions/Triggers/GameObjectActiveTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace FingerFighter.Control.Common.Conditions.Triggers
+{
+    public class GameObjectActiveTrigger : ATrigger
+    {
+        [SerializeField] private GameObject subject;
+        
+        protected override void InvokeTrigger(bool flagIsOn) 
+            => subject.SetActive(flagIsOn);
+    }
+}
